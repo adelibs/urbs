82ed78712856e29830ecbfcc43671573bb200c95
Sissi Bazan <adeli.bazan@tum.de>
2018-01-14 16:18:52 +0100

    everything

---
+++ excel_tomatoS1-S13/runme_EWF.py
@@ -0,0 +1,185 @@
+import os
+import pandas as pd
+import pyomo.environ
+import shutil
+import urbs
+from datetime import datetime
+from pyomo.opt.base import SolverFactory
+
+
+# SCENARIOS
+def scenario_base(data):
+    #sto = data['storage']
+    #sto.loc[('StRupertMayer', 'Battery'), 'cap-up-c'] = 0
+    #pro = data['process']
+    #pro.loc[('StRupertMayer', 'Photovoltaics'), 'cap-up'] = 0
+return data
+
+
+def prepare_result_directory(result_name):
+    """ create a time stamped directory within the result folder """
+    # timestamp for result directory
+    now = datetime.now().strftime('%Y%m%dT%H%M')
+
+    # create result directory if not existent
+    result_dir = os.path.join('result', '{}-{}'.format(result_name, now))
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+
+    return result_dir
+
+
+def setup_solver(optim, logfile='solver.log'):
+    """ """
+    if optim.name == 'gurobi':
+        # reference with list of option names
+        # http://www.gurobi.com/documentation/5.6/reference-manual/parameters
+        optim.set_options("logfile={}".format(logfile))
+        # optim.set_options("timelimit=7200")  # seconds
+        # optim.set_options("mipgap=5e-4")  # default = 1e-4
+    elif optim.name == 'glpk':
+        # reference with list of options
+        # execute 'glpsol --help'
+        optim.set_options("log={}".format(logfile))
+        # optim.set_options("tmlim=7200")  # seconds
+        # optim.set_options("mipgap=.0005")
+    else:
+        print("Warning from setup_solver: no options set for solver "
+              "'{}'!".format(optim.name))
+    return optim
+
+
+def run_scenario(input_file, timesteps, scenario, result_dir,
+                 plot_tuples=None, plot_periods=None, report_tuples=None):
+    """ run an urbs model for given input, time steps and scenario
+
+    Args:
+        input_file: filename to an Excel spreadsheet for urbs.read_excel
+        timesteps: a list of timesteps, e.g. range(0,8761)
+        scenario: a scenario function that modifies the input data dict
+        result_dir: directory name for result spreadsheet and plots
+        plot_tuples: (optional) list of plot tuples (c.f. urbs.result_figures)
+        plot_periods: (optional) dict of plot periods (c.f. urbs.result_figures)
+        report_tuples: (optional) list of (sit, com) tuples (c.f. urbs.report)
+
+    Returns:
+        the urbs model instance
+    """
+
+    # scenario name, read and modify data for scenario
+    sce = scenario.__name__
+    data = urbs.read_excel(input_file)
+    data = scenario(data)
+
+    # create model
+    prob = urbs.create_model(data, timesteps)
+
+    # refresh time stamp string and create filename for logfile
+    now = prob.created
+    log_filename = os.path.join(result_dir, '{}.log').format(sce)
+
+    # solve model and read results
+    optim = SolverFactory('gurobi', solver_io="python")  # cplex, glpk, gurobi, ...
+    optim = setup_solver(optim, logfile=log_filename)
+    result = optim.solve(prob, tee=True)
+
+    # save problem solution (and input data) to HDF5 file
+   # urbs.save(prob, os.path.join(result_dir, '{}.h5'.format(sce)))
+
+    # write report to spreadsheet
+    urbs.report(
+        prob,
+        os.path.join(result_dir, '{}.xlsx').format(sce),
+        report_tuples=report_tuples)
+
+    # result plots
+    urbs.result_figures(
+        prob,
+        os.path.join(result_dir, '{}'.format(sce)),
+        plot_title_prefix=sce.replace('_', ' '),
+        plot_tuples=plot_tuples,
+        periods=plot_periods,
+        figure_size=(24, 9))
+    return prob
+
+if __name__ == '__main__':
+    input_file = 'dummy_EWF.xlsx'
+    result_name = os.path.splitext(input_file)[0]  # cut away file extension
+    result_dir = prepare_result_directory(result_name)  # name + time stamp
+
+    # copy input file to result directory
+    shutil.copyfile(input_file, os.path.join(result_dir, input_file))
+    # copy runme.py to result directory
+    shutil.copyfile(__file__, os.path.join(result_dir, 'runme_EWF.py'))
+
+    # simulation timesteps
+    (offset, length) = (0,8760)  # time step selection
+    timesteps = range(offset, offset+length+1)
+
+    # plotting commodities/sites
+    plot_tuples = [
+       ('StRupertMayer', 'Elec'),
+       ('StRupertMayer', 'Diesel'), 
+       ('StRupertMayer', 'Solar'),
+       ('StRupertMayer', 'Water'),
+       ('StRupertMayer', 'WaterBuy'),
+       ('StRupertMayer', 'Tomato'),
+       ('StRupertMayer', 'TomatoBuy'),
+       ('StRupertMayer', 'Irrigation Water'),
+       ('StRupertMayer', 'Tomato Waste'),
+       ('StRupertMayer', 'Biogas Equivalent'),
+       ('StRupertMayer', 'Biogas'),
+       ('StRupertMayer', 'TomatoSell'),
+       ]
+    
+    # detailed reporting commodity/sites
+    report_tuples = [
+       ('StRupertMayer', 'Elec'),
+       ('StRupertMayer', 'Diesel'),        
+       ('StRupertMayer', 'Solar'),  
+       ('StRupertMayer', 'Water'),
+       ('StRupertMayer', 'WaterBuy'),
+       ('StRupertMayer', 'Tomato'),
+       ('StRupertMayer', 'TomatoBuy'),
+       ('StRupertMayer', 'Irrigation Water'),
+       ('StRupertMayer', 'Tomato Waste'),
+       ('StRupertMayer', 'TomatoSell'),
+       ('StRupertMayer', 'Biogas Equivalent'),
+       ('StRupertMayer', 'Biogas'),
+       ]
+
+    # plotting timesteps
+    plot_periods = {
+       '1_week': range(   1,  168),
+        }
+
+    # add or change plot colors
+    my_colors = {
+        'Demand': (0, 0, 0),
+        'Diesel Generator': (89, 89, 89),
+        'Photovoltaics': (255,201,71),
+        'WaterBuy to Water':(200,221,241),
+        'Water to Irrigation Water':(36,91,140),
+        'TomatoBuy to Tomato':(229,152,154),
+        'Biogas Generator':(172,233,110),
+        'Biogas Digester':(54,93,14),    
+        'Tomato Field':(213,91,94),
+        'Water Pump':(91, 155, 213),
+        'Shunt(Biogas Equivalent)':(171,143,110),
+        'Tomato to TomatoSell':(121,31,34),
+        'Tomato Waste to Biogas Equivalent':(130,105,75),
+        'Tomato to Biogas Equivalent':(118,68,90),
+        }
+    for country, color in my_colors.items():
+        urbs.COLORS[country] = color
+
+    # select scenarios to be run
+    scenarios = [
+        scenario_base,
+     ]
+
+    for scenario in scenarios:
+        prob = run_scenario(input_file, timesteps, scenario, result_dir,
+                            plot_tuples=plot_tuples,
+                            plot_periods=plot_periods,
+                            report_tuples=report_tuples)
---
+++ old_excel/old input excel/runme_dummy.py
@@ -0,0 +1,174 @@
+import os
+import pandas as pd
+import pyomo.environ
+import shutil
+import urbs
+from datetime import datetime
+from pyomo.opt.base import SolverFactory
+
+
+# SCENARIOS
+def scenario_base(data):
+    #sto = data['storage']
+    #sto.loc[('StRupertMayer', 'Battery'), 'cap-up-c'] = 0
+    #sto.loc[('StRupertMayer', 'Water Tank'), 'cap-up-c'] = 0
+    #pro = data['process']
+    #pro.loc[('StRupertMayer', 'Photovoltaics'), 'cap-up'] = 0
+    #pro.loc[('StRupertMayer', 'Diesel Generator'), 'cap-up'] = 0
+    return data
+
+
+def prepare_result_directory(result_name):
+    """ create a time stamped directory within the result folder """
+    # timestamp for result directory
+    now = datetime.now().strftime('%Y%m%dT%H%M')
+    
+    # create result directory if not existent
+    result_dir = os.path.join('result', '{}-{}'.format(result_name, now))
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+
+    return result_dir
+
+
+def setup_solver(optim, logfile='solver.log'):
+    """ """
+    if optim.name == 'gurobi':
+        # reference with list of option names
+        # http://www.gurobi.com/documentation/5.6/reference-manual/parameters
+        optim.set_options("logfile={}".format(logfile))
+        # optim.set_options("timelimit=7200")  # seconds
+        # optim.set_options("mipgap=5e-4")  # default = 1e-4
+    elif optim.name == 'glpk':
+        # reference with list of options
+        # execute 'glpsol --help'
+        optim.set_options("log={}".format(logfile))
+        # optim.set_options("tmlim=7200")  # seconds
+        # optim.set_options("mipgap=.0005")
+    else:
+        print("Warning from setup_solver: no options set for solver "
+              "'{}'!".format(optim.name))
+    return optim
+
+
+def run_scenario(input_file, timesteps, scenario, result_dir,
+                 plot_tuples=None, plot_periods=None, report_tuples=None):
+    """ run an urbs model for given input, time steps and scenario
+
+    Args:
+        input_file: filename to an Excel spreadsheet for urbs.read_excel
+        timesteps: a list of timesteps, e.g. range(0,8761)
+        scenario: a scenario function that modifies the input data dict
+        result_dir: directory name for result spreadsheet and plots
+        plot_tuples: (optional) list of plot tuples (c.f. urbs.result_figures)
+        plot_periods: (optional) dict of plot periods (c.f. urbs.result_figures)
+        report_tuples: (optional) list of (sit, com) tuples (c.f. urbs.report)
+
+    Returns:
+        the urbs model instance
+    """
+
+    # scenario name, read and modify data for scenario
+    sce = scenario.__name__
+    data = urbs.read_excel(input_file)
+    data = scenario(data)
+
+    # create model
+    prob = urbs.create_model(data, timesteps)
+
+    # refresh time stamp string and create filename for logfile
+    now = prob.created
+    log_filename = os.path.join(result_dir, '{}.log').format(sce)
+
+    # solve model and read results
+    optim = SolverFactory('gurobi', solver_io="python")  # cplex, glpk, gurobi, ...
+    optim = setup_solver(optim, logfile=log_filename)
+    result = optim.solve(prob, tee=True)
+
+    # save problem solution (and input data) to HDF5 file
+   # urbs.save(prob, os.path.join(result_dir, '{}.h5'.format(sce)))
+
+    # write report to spreadsheet
+    urbs.report(
+        prob,
+        os.path.join(result_dir, '{}.xlsx').format(sce),
+        report_tuples=report_tuples)
+
+    # result plots
+    urbs.result_figures(
+        prob,
+        os.path.join(result_dir, '{}'.format(sce)),
+        plot_title_prefix=sce.replace('_', ' '),
+        plot_tuples=plot_tuples,
+        periods=plot_periods,
+        figure_size=(24, 9))
+    return prob
+
+if __name__ == '__main__':
+    input_file = 'dummy_example_field_tomato.xlsx'
+    result_name = os.path.splitext(input_file)[0]  # cut away file extension
+    result_dir = prepare_result_directory(result_name)  # name + time stamp
+
+    # copy input file to result directory
+    shutil.copyfile(input_file, os.path.join(result_dir, input_file))
+    # copy runme.py to result directory
+    shutil.copyfile(__file__, os.path.join(result_dir, 'runme_dummy.py'))
+
+    # simulation timesteps
+    (offset, length) = (0,2000)  # time step selection
+    timesteps = range(offset, offset+length+1)
+
+    # plotting commodities/sites
+    plot_tuples = [
+        ('StRupertMayer', 'Elec'),
+        ('StRupertMayer', 'Water'), 
+       # ('StRupertMayer', 'Water Power'), 
+        ('StRupertMayer', 'Tomato ha'),
+        ('StRupertMayer', 'Tomato'),
+       # ('StRupertMayer', 'Solar'),    
+        ]
+
+    # detailed reporting commodity/sites
+    report_tuples = [
+       ('StRupertMayer', 'Elec'),
+       #('StRupertMayer', 'Diesel'),        
+       #('StRupertMayer', 'Solar'), 
+       ('StRupertMayer', 'Water'),
+       ('StRupertMayer', 'Water Power'),
+       ('StRupertMayer', 'Tomato ha'),
+       ('StRupertMayer', 'Tomato'),
+       ]
+
+    # plotting timesteps
+    plot_periods = {
+        'all': timesteps[0:2000],
+        '1_week': range(  1592,  1760),
+      #'1_day': range(   1,  24),
+        #'1_month': range(   1,  720),
+    }
+
+    # add or change plot colors
+    my_colors = {
+        'Storage':(4,0,98),   
+        'Demand': (0, 0, 0),
+        'Diesel Generator': (89, 89, 89),
+        'Photovoltaics': (255,201,71),
+        'Water Pump':(91, 155, 213),
+        'Wheat Field':(213,149,91),
+        'Converter kWh to m3':(91,213,210),
+        'Converter ha to ton':(213,91,94),
+        }
+    
+    for country, color in my_colors.items():
+        urbs.COLORS[country] = color
+
+    # select scenarios to be run
+    scenarios = [
+        scenario_base,
+     ]
+
+    for scenario in scenarios:
+        prob = run_scenario(input_file, timesteps, scenario, result_dir,
+                            plot_tuples=plot_tuples,
+                            plot_periods=plot_periods,
+                            report_tuples=report_tuples)
